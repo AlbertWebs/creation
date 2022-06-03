c8a5f8463bfb3285c0bb1ccc111742aef9bb9a4d
AlbertWebs <albertmuhatia@gmail.com>
2022-06-03 09:28:49 +0300

    Update

--- resources/views/front/schema.blade.php
+++ resources/views/front/schema.blade.php
@@ -1,19 +1,19 @@
 <script type='application/ld+json'>
-    {
+{
       "@context": "http://www.schema.org",
       "@type": "ProfessionalService",
       "name": "Creation Offcie Fitouts",
       "url": "http://creationltd.co.ke/",
-      "logo": "https://creationltd.co.ke/uploads/logo/amaniCropped.png",
+      "logo": "https://creationltd.co.ke/uploads/logo/creatio.jpg",
       "sameAs": [ 
         "https://web.facebook.com/creationltd/",
         "https://www.instagram.com/creation_office_fitout/",
         "https://www.linkedin.com/company/creation-office-fitouts/",
-        "https://twitter.com/creationoffice1",
+        "https://twitter.com/creationoffice1"
         
       ],
       "priceRange": "$$$$",
-      "image": "https://creationltd.co.ke/uploads/logo/creatio_logo.png",
+      "image": "https://creationltd.co.ke/uploads/portfolio/2021-02-2408:27:13imageGTBankbank6interiorDesign.jpg",
       "description": "Creation office Fitouts is the number one interior design firm, in Nairobi, Kenya.We are professionals in Interior Design, Construction, Refurbishments, Partitioning, Ceiling and Flooring, Furniture Supply.",
       "address": {
          "@type": "PostalAddress",
@@ -25,5 +25,5 @@
       },
        "openingHours": "Mo 01:00-01:00 Tu 01:00-01:00 We 01:00-01:00 Th 01:00-01:00 Fr 01:00-01:00 Sa 01:00-01:00 Su 01:00-01:00",
        "telephone": "+254 723 768 593"
-    }
+}
 </script>
